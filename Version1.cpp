b9914c8dae101ad59142f3d96c6a1900ff625f72
JesseGoncalves <goncalve@seattleu.edu>
2017-05-30 18:24:58 -0500

    Update Version1.cpp

--- Version1.cpp
+++ Version1.cpp
@@ -4,56 +4,38 @@
 // then multiplies result by 4 to estimate pi
 // test precision by running code 10 times for 10^n trials,
 // where n={1,2,3,4,5,6}, and calculating standard deviations
-#include<cstdlib>
-#include<cmath>
-#include<iostream>
-#include<vector>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include "vectorStandardDev.h"
+#include "monteCarloPi.h"
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
   int nTrials; // number of trials
+  srand(time(0)); // initialize seed for random number generator
 
-  for (nTrials = 10; nTrials < 1000001; nTrials *= 10) {  // increment number of trials
+  for (nTrials = 10; nTrials < 100000001; nTrials *= 10) {  // increment number of trials
 	  
     vector<double> estimates; // vector of estimates of pi
 	  
     for (int j = 0; j < 10; ++j) {  // repeat 10 times
 
-      // generate and classify random points
-      int results = 0; // number of hits in circle
-      for (int i = 0; i < nTrials; ++i) {
-        double x = rand() / (double)RAND_MAX; // x coord
-        double y = rand() / (double)RAND_MAX; // y coord
-        double distance = sqrt(pow(x, 2) + pow(y, 2)); // distance from origin
-        if (distance <= 1) {
-          ++results;
-        }
-      }
- 
-      // estimate area of quarter circle
-      double quarterCircleArea = (double)results / (double)nTrials; 
-
-      // calculate estimation of pi
-      double piEstimate = quarterCircleArea * 4;
+      // estimate value of pi
+      double piEstimate;
+      piEstimate = monteCarloPi(nTrials);
     
       // create vector of estimates
       estimates.push_back(piEstimate);
     }
-    
-    // calculate mean and variance
-    double mean = 0;  // mean of estimates
-    for (int a = 0; a < 10; ++a) {
-      mean += estimates[a] / 10;
-    }
-    double variance = 0;  // variance of estimates
-    for (int b = 0; b < 10; ++b) {
-      variance += pow(estimates[b] - mean, 2) / 10;
-    }
-	  
+
     // calculate and display standard deviation
-    double standardDev = pow(variance, .5);
+    double standardDev;
+    standardDev = vectorStandardDev(estimates);
     cout << "For " << nTrials << " trials the standard deviation is " << standardDev << endl;
   }
 
