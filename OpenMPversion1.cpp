9793905ed9880e4fca0b451f2fad79ef5c5728dd
Zach Byerly <zbyerly@cct.lsu.edu>
2017-05-29 19:07:24 -0500

    debugging openmp program

--- OpenMPversion1.cpp
+++ OpenMPversion1.cpp
@@ -11,30 +11,39 @@ using namespace std;
 
 int main()
 {
-	// gets user input for number of trials
-	cout << "Enter number of trials: " << endl;
-	int nTrials;
-	cin >> nTrials;
-		
-	// generate and classify random points in parallel
-	int results = 0;
-  	double x,y,distance;
-	#pragma omp parallel for private(x,y,distance) shared(results)
-  	for (int i = 0; i != nTrials; ++i) {
-		x = rand() / (double)RAND_MAX;
-		y = rand() / (double)RAND_MAX;
-		distance = sqrt(pow(x, 2) + pow(y, 2));
-		if (distance <= 1) {
-			++results;
-		}
-	}
+  int nthreads;
 
-	// estimate area of quarter circle
-	double qcircA = (double)results / (double)nTrials;
+  // gets user input for number of trials
+  cout << "Enter number of trials: " << endl;
+  int nTrials; // total number of trials
+  cin >> nTrials;
 
-	// calculate and display estimation of pi
-	double mcPi = qcircA * 4;
-	cout << "Estimate of pi = " << mcPi << endl;
+  // generate and classify random points in parallel
+  int results = 0; // hits inside circle
+  double x; // x coord
+  double y; // y coord
+  double distance; // distance from center of circle
+#pragma omp parallel for private(x,y,distance,nthreads) shared(results)
+  for (int i = 0; i < nTrials; ++i) {
 
-	return 0;
+    // print number of threads
+    nthreads = omp_get_num_threads();
+    cout << "number of threads: " << nthreads << endl;
+
+    x = rand() / (double)RAND_MAX;
+    y = rand() / (double)RAND_MAX;
+    distance = sqrt(pow(x, 2) + pow(y, 2));
+    if (distance <= 1) {
+      ++results;
+    }
+  }
+
+  // estimate area of quarter circle
+  double qcircA = (double)results / (double)nTrials;
+
+  // calculate and display estimation of pi
+  double mcPi = qcircA * 4;
+  cout << "Estimate of pi = " << mcPi << endl;
+
+  return 0;
 }
