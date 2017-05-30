1ab442765203128319fc1fc64f2b070322a7a054
JesseGoncalves <goncalve@seattleu.edu>
2017-05-30 14:11:53 -0500

    Update OpenMPversion1.cpp

--- OpenMPversion1.cpp
+++ OpenMPversion1.cpp
@@ -1,42 +1,61 @@
-// estimate pi using monte carlo methods, parallelized w/ OpenMP
+// estimate pi using monte carlo methods
 // calculates probability of random point 0 <= x,y < 1 being
 // inside a quarter of a circle of radius 1
 // then multiplies result by 4 to estimate pi
+// test precision by running code 10 times for 10^n trials,
+// where n={1,2,3,4,5,6}, and calculating standard deviations
 #include<cstdlib>
 #include<cmath>
 #include<iostream>
-#include<omp.h>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-  // gets user input for number of trials
-  cout << "Enter number of trials: " << endl;
-  int nTrials; // total number of trials
-  cin >> nTrials;
+  int nTrials; // number of trials
 
-  // generate and classify random points in parallel
-  int results = 0; // hits inside circle
-  double x; // x coord
-  double y; // y coord
-  double distance; // distance from center of circle
-  #pragma omp parallel for private(x,y,distance,nthreads) shared(results)
-  for (int i = 0; i < nTrials; ++i) {
-    x = rand() / (double)RAND_MAX;
-    y = rand() / (double)RAND_MAX;
-    distance = sqrt(pow(x, 2) + pow(y, 2));
-    if (distance <= 1) {
-      ++results;
+  for (nTrials = 10; nTrials < 100000001; nTrials *= 10) {  // increment number of trials
+	  
+    vector<double> estimates; // vector of estimates of pi
+	  
+    for (int j = 0; j < 10; ++j) {  // repeat 10 times
+
+      // generate and classify random points
+      int results = 0; // number of hits in circle
+      for (int i = 0; i < nTrials; ++i) {
+        double x = rand() / (double)RAND_MAX; // x coord
+        double y = rand() / (double)RAND_MAX; // y coord
+        double distance = sqrt(pow(x, 2) + pow(y, 2)); // distance from origin
+        if (distance <= 1) {
+          ++results;
+        }
+      }
+ 
+      // estimate area of quarter circle
+      double quarterCircleArea = (double)results / (double)nTrials; 
+
+      // calculate estimation of pi
+      double piEstimate = quarterCircleArea * 4;
+    
+      // create vector of estimates
+      estimates.push_back(piEstimate);
     }
+    
+    // calculate mean and variance
+    double mean = 0;  // mean of estimates
+    for (int a = 0; a < 10; ++a) {
+      mean += estimates[a] / 10;
+    }
+    double variance = 0;  // variance of estimates
+    for (int b = 0; b < 10; ++b) {
+      variance += pow(estimates[b] - mean, 2) / 10;
+    }
+	  
+    // calculate and display standard deviation
+    double standardDev = pow(variance, .5);
+    cout << "For " << nTrials << " trials the standard deviation is " << standardDev << endl;
   }
-
-  // estimate area of quarter circle
-  double quarterCircleArea = (double)results / (double)nTrials;
-
-  // calculate and display estimation of pi
-  double piEstimate = quarterCircleArea * 4;
-  cout << "Estimate of pi = " << piEstimate << endl;
 
   return 0;
 }
