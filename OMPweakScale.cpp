5a661c11ba42eae3e1ec9c690aa710f5a8327e54
Jesse <goncalve@seattleu.edu>
2017-06-12 15:56:45 -0500

    replace with better version

--- OMPweakScale.cpp
+++ OMPweakScale.cpp
@@ -2,7 +2,7 @@
 // calculates probability of random point 0 <= x,y < 1 being
 // inside a quarter of a circle of radius 1
 // then multiplies result by 4 to estimate pi
-// compile with: g++ OMPweakScale.cpp -o ompWeak -fopenmp -std=c++11
+// compile with: g++ OMPweakScale2.cpp -o ompWeak2 -fopenmp -std=c++11
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
@@ -14,42 +14,46 @@
 
 int main()
 {
-  for (int k = 1; k < 17; ++k) {
-    auto start = std::chrono::steady_clock::now();  // gets time at beginning of program
-    int numThreads = k;  // number of threads for parallel section
-    int threadNum;  // number for each thread
-    unsigned long long numTrials = 1000000000;  // number of trials
-    double x;  // random x coordinates
-    double y;  // random y coordinates
-    unsigned long long numHits;  // number of hits inside quarter circle
-    double piEstimate;  // estimate of pi
-    int i; int j;  // for loop counters
-    unsigned int seed;  // seed for random number generator
-    std::vector<double> estimateVec(numThreads, 0);  // vector of zeros to hold estimates of pi
-    //#pragma omp parallel proc_bind(close) num_threads(numThreads)
-    #pragma omp parallel for num_threads(numThreads) private(i,j,threadNum,seed,x,y,numHits,piEstimate)
-    for (i = 0; i < numThreads; ++i) {  // run one simulation on each thread
-      threadNum = omp_get_thread_num();  // gets number of thread
-      seed = threadNum;  // sets different seed for each thread
-      numHits = 0;  // resets number of hits
-      for (j = 0; j < numTrials; ++j) {
-        x = rand_r(&seed) / (double)RAND_MAX;  // generate random x coordinate
-        y = rand_r(&seed) / (double)RAND_MAX;  // generate random y coordinate
-        if (x * x + y * y <= 1) {  // check if inside quarter circle
-          ++numHits;  // +1 hit inside circle
-        }
+  unsigned long long numTrials = 100000000;  // number of trials
+  int numThreads;  // number of threads for parallel section
+  int threadNum;  // number for each thread
+  unsigned long long numHits;  // number of hits inside quarter circle
+  double piEstimate;  // estimate of pi
+  unsigned int xSeed, ySeed;  // seeds for random number generator
+  unsigned long long i, k; int j, l;  // for loop counters
+  for (numThreads = 1; numThreads < 17; ++numThreads) {
+    std::vector<double> randomXvec(numTrials * numThreads);  // vector for x coordinates
+    std::vector<double> randomYvec(numTrials * numThreads);  // vector for y coordinates
+    std::vector<double> estimateVec(numThreads);  // vector for estimates of pi
+    for (l = 0; l < 10; ++l) {
+      xSeed = time(0); ySeed = numThreads;  // initialize seeds
+      for (i = 0; i < numTrials * numThreads; ++i) {
+        randomXvec[i] = rand_r(&xSeed) / (double)RAND_MAX;  // generate x coordinate
+        randomYvec[i] = rand_r(&ySeed) / (double)RAND_MAX;  // generate y coordinate
       }
-      piEstimate = numHits / (double)numTrials * 4;  // estimates value of pi
-      estimateVec[threadNum] = piEstimate;  // save estimate to estimateVec
+      auto start = std::chrono::steady_clock::now();  // gets time at beginning of program
+      //#pragma omp parallel proc_bind(close) num_threads(numThreads)
+      #pragma omp parallel for num_threads(numThreads) private(j,k,threadNum,numHits,piEstimate)
+      for (j = 0; j < numThreads; ++j) {  // run one simulation on each thread
+        threadNum = omp_get_thread_num();  // gets number of thread
+        numHits = 0;  // resets number of hits
+        for (k = threadNum * numTrials; k < (threadNum + 1) * numTrials; ++k) {
+          if (randomXvec[k] * randomXvec[k] + randomYvec[k] * randomYvec[k] <= 1) {  // check if inside quarter circle
+            ++numHits;  // +1 hit inside circle
+          }
+        }
+        piEstimate = numHits / (double)numTrials * 4;  // estimates value of pi
+        estimateVec[threadNum] = piEstimate;  // save estimate to estimateVec
+      }
+      double mean = 0;  // mean of estimates
+      for (int a = 0; a < numThreads; ++a) {
+        mean += estimateVec[a] / numThreads;
+      }
+      auto end = std::chrono::steady_clock::now();  // gets time at end of program
+      std::chrono::duration<double> execTime = end - start;  // calculates execution time
+      std::ofstream resultFile("OMPweakScaling2.csv", std::ios::app);  // opens file to append results
+      resultFile << mean << ", " << numTrials << ", " << numThreads << ", " << execTime.count() << std::endl;  // write results to file
     }
-    double mean = 0;  // mean of estimates
-    for (int a = 0; a < numThreads; ++a) {
-      mean += estimateVec[a] / numThreads;
-    }
-    auto end = std::chrono::steady_clock::now();  // gets time at end of program
-    std::chrono::duration<double> execTime = end - start;  // calculates execution time
-    std::ofstream resultFile("OMPweakScaling.csv", std::ios::app);  // opens file to append results
-    resultFile << mean << ", " << numTrials << ", " << numThreads << ", " << execTime.count() << std::endl;  // write results to file
   }
   return 0;
 }
